5f175d1f061c77b778a5c0546aa9d3b0d36a2578
darrin allen <darrin.lovard.allen@gmail.com>
2022-12-26 11:47:46 -0800

    codegrade

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ axios.get('https://swapi.dev/api/people')
     <div className="App">
       <h1 className="Header">Characters</h1>
       <div>{data.map((film,id) => <p>{film}</p>)}</div>
-
     </div>
   );
   
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,16 +1,2 @@
 // Write your Character component here
-import { render } from '@testing-library/react'
-import React, {useEffect, useState} from 'react'
-
-export default function Character(props) {
-   const {finalData}= props
-  let movies = props.names
-
-    
-   
-   return(
-        
-null   
- )  
-    
-}
+//i didnt use this
